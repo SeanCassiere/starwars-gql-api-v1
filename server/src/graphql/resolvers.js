41bae3f792402b359eff1dfb82206e87aeb4fe3d
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2021-02-28 15:39:30 +0530

    Test Routes on Server

--- server/src/graphql/resolvers.js
+++ server/src/graphql/resolvers.js
@@ -1,8 +1,14 @@
+import { AuthenticationError } from "apollo-server-express";
 import axios from "axios";
 
 const resolvers = {
 	Query: {
+		// Test Routes
 		hello: () => "Hello world!",
+		authenticationError: (parent, args, context) => {
+			throw new AuthenticationError("must authenticate");
+		},
+		// Main Routes
 	},
 };
 
